fae3c3bcd3f61cf727eddd3bcbef8b3e21a69d42
Khobe <chuksleo27@gmail.com>
2023-09-08 19:51:22 +0100

    Files Uploaded

---
+++ script.js
@@ -0,0 +1,14 @@
+const weekdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+function day() {
+const d = new Date();
+let day = weekdays[d.getDay()];
+document.getElementById("currentDay").innerHTML = day;
+}
+day();
+function utcTime() {
+const date = new Date();
+let ms = date.getTime()
+document.getElementById('utcTime').innerHTML = ms;
+}
+utcTime();
